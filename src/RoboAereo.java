14bcbce05c1e5e03b66ca737eac39fc6c432ddfd
leticia nakada <l247337@dac.unicamp.br>
2025-03-20 19:32:43 -0300

    adicionando classe RoboAereo

--- src/RoboAereo.java
+++ src/RoboAereo.java
@@ -1,2 +1,26 @@
-public class RoboAereo {
+public class RoboAereo extends Robo{
+    int altitude;
+    int altitudeMaxima;
+
+    public RoboAereo(String nome, int posicaoX, int posicaoY, int altitude, int altitudeMaxima) {
+        super(nome, posicaoX, posicaoY);
+        this.altitude = altitude;
+        this.altitudeMaxima = altitudeMaxima;
+    }
+
+    public void subir(int metros) {
+        if ((altitude + metros) > altitudeMaxima) {
+            altitude = altitudeMaxima;
+        } else {
+            altitude += metros;
+        }
+    }
+
+    public void descer(int metros) {
+        if ((altitude - metros) < 0) {
+            altitude = 0;
+        } else {
+            altitude -= metros;
+        }
+    }
 }
